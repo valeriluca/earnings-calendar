b021e200bed35d4ec8cf38f2a71178282ceee4af
valeriluca <valeriluca@example.com>
2025-11-18 18:22:58 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.earningscal.app',
+  appName: 'earnings-calendar',
+  webDir: 'www'
+};
+
+export default config;
